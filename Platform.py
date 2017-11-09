4011cffa53823fc7bead2aed227a3c7863bb7766
OumaymaMessoussi <oumayma.messoussi@gmail.com>
2017-11-09 12:14:26 -0500

    added the level file

--- Platform.py
+++ Platform.py
@@ -1,1 +1,8 @@
-class Platform:
+import pygame
+
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self):
+        super.__init__()
+        self.image = pygame.image.load('platform.png').convert()
+        self.rect = self.image.get_rect()
