661f62c45932a1b1ad823976e84ee854dccfd668
AmellaPirano <samantha.puckett@live.com>
2017-11-09 11:48:07 -0500

    Enemy sub-class skeletons added. Getters for enemy attributes added.

--- Enemy.py
+++ Enemy.py
@@ -1,7 +1,6 @@
-#Abstract class
+#Abstract class - do not instantiate
 class Enemy():
-    def __init__(self, name, type, damage):
-        self.name = name
+    def __init__(self, type, damage):
         self.type = type
         self.damage = damage
         self.alive = True
@@ -9,5 +8,29 @@ class Enemy():
     def hitPlayer(self, player):
         player.hit(self.damage)
 
-    def hit(self):
+    def gotHit(self):
         self.alive = False
+
+    def getType(self):
+        return self.type
+    def getDamage(self):
+        return self.damage
+    def isAlive(self):
+        return self.alive
+
+#Sub-classes
+class BasicEnemy(Enemy):
+    def __init__(self):
+        Enemy.__init__(self, "Basic Enemy", 1)
+
+class ShootingEnemy(Enemy):
+    def __init__(self):
+        Enemy.__init__(self, "Shooting Enemy", 2)
+        range = 10 #Change once we figure out how to work out distances
+
+    def getRange(self):
+        return self.range
+
+class BossEnemy(Enemy):
+    def __init__(self):
+        Enemy.__init__(self, "Boss Enemy", 3)
