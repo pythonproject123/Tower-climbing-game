6d07e66a7b410678b9d2f5415a405c55b8a3a7a3
OumaymaMessoussi <oumayma.messoussi@gmail.com>
2017-11-26 12:08:58 -0500

    added the spritesheet file

--- Platform.py
+++ Platform.py
@@ -1,8 +1,12 @@
-import pygame
+#source of tiles file: https://opengameart.org/content/platformer-art-deluxe
+
+from SpriteSheets import *
 
 
 class Platform(pygame.sprite.Sprite):
+
     def __init__(self):
         super.__init__()
-        self.image = pygame.image.load('platform.png').convert()
+        sprite_sheet = SpriteSheet('tiles_spritesheet.png')
+        self.image = sprite_sheet.get_image(14*8, 14*6, 14, 14)
         self.rect = self.image.get_rect()
---
+++ SpriteSheets.py
@@ -0,0 +1,18 @@
+#This class is used to grab individual sprites from sprite sheets.
+
+import pygame
+
+
+class SpriteSheet(object):
+    def __init__(self, file_name):
+        self.sprite_sheet = pygame.image.load(file_name).convert()
+
+    def get_image(self, x, y, width, height):
+        """ Grab a single image out of a larger spritesheet
+            Pass in the x, y location of the sprite
+            and the width and height of the sprite. """
+
+        image = pygame.Surface([width, height]).convert()
+        image.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
+        image.set_colorkey(0, 0, 0)
+        return image
