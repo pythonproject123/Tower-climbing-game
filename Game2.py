c116562255d5fa5da0330bf1365c63c08a32923e
rc15t <33007296+rc15t@users.noreply.github.com>
2017-12-08 09:18:53 -0500

    Add files via upload

---
+++ Game2.py
@@ -0,0 +1,165 @@
+from Player import *
+import level
+import time
+
+black = (0,0,0)
+white = (255,255,255)
+red = (200,0,0)
+bright_red = (255,0,0)
+green = (0,200,0)
+bright_green = (0,255,0)
+display_width = 800
+display_height = 600
+screen = pygame.display.set_mode((display_width,display_height))
+
+
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(screen, ac,(x,y,w,h))
+
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    smallText = pygame.font.SysFont("comicsansms",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    screen.blit(textSurf, textRect)
+
+def game_intro():
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        screen.fill(white)
+        largeText = pygame.font.SysFont("comicsansms", 115)
+        TextSurf, TextRect = text_objects("Time 2 play the game", largeText)
+        TextRect.center = ((display_width / 2), (display_height / 2))
+        screen.blit(TextSurf, TextRect)
+
+        button("Username", 150, 450, 100, 50, green, bright_green, main)
+        button("Quit", 550, 450, 100, 50, red, bright_red, quitgame)
+
+        pygame.display.update()
+        clock.tick(15)
+
+def quitgame():
+    pygame.quit()
+    quit()
+
+def text_objects(text,font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def main():
+    """ Main Program """
+    pygame.init()
+
+    # Set the height and width of the screen
+
+    # Create the player
+    player = Player('test')
+
+    # Create all the levels
+    level_list = [level.LevelOne(player)]
+    #level_list.append(level.LevelTwo(player))
+
+    # Set the current level
+    current_level_no = 0
+    current_level = level_list[current_level_no]
+
+    active_sprite_list = pygame.sprite.Group()
+    player.level = current_level
+
+    player.rect.x = 340
+    player.rect.y = 600 - player.rect.height
+    active_sprite_list.add(player)
+
+    # Loop until the user clicks the close button.
+    done = False
+
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
+
+    # Game loop
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True  # Flag that we are done so we exit this loop
+            if not player.alive:
+                pygame.font.init()
+                font = pygame.font.SysFont('Comic Sans MS', 30)
+                textSurf = font.render('Game Over!', True, (0, 0, 0))
+                textRect = textSurf.get_rect()
+                textRect.center = (400, 300)
+                screen.blit(textSurf, textRect)
+                pygame.display.update()
+                time.sleep(2)
+                done = True
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.go_left()
+                if event.key == pygame.K_RIGHT:
+                    player.go_right()
+                if event.key == pygame.K_SPACE:
+                    player.jump()
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT and player.change_x < 0:
+                    player.stop()
+                if event.key == pygame.K_RIGHT and player.change_x > 0:
+                    player.stop()
+
+        active_sprite_list.update()
+        current_level.update()
+
+        # If the player gets near the right side, shift the world left (-x)
+        if player.rect.right >= 500:
+            diff = player.rect.right - 500
+            player.rect.right = 500
+            current_level.shift_world(-diff)
+
+        # If the player gets near the left side, shift the world right (+x)
+        if player.rect.left <= 120:
+            diff = 120 - player.rect.left
+            player.rect.left = 120
+            current_level.shift_world(diff)
+
+        # If the player wins the level, go to the next level
+        if player.won:
+            player.rect.x = 120
+            player.reset()
+            if current_level_no < len(level_list) - 1:
+                current_level_no += 1
+                current_level = level_list[current_level_no]
+                player.level = current_level
+            else:
+                done = True
+
+        # ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT
+        current_level.draw(screen)
+        active_sprite_list.draw(screen)
+        # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
+
+        # Limit to 60 frames per second
+        clock.tick(60)
+
+        # Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
+
+    # Be IDLE friendly. If you forget this line, the program will 'hang'
+    # on exit.
+    pygame.quit()
+
+if __name__ == "__main__":
+    game_intro()
